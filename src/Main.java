25a0b4c4d3dde8defc3e2b18cff8906fbf94fa05
luciana furtado <lucianafurtado10@gmail.com>
2022-07-19 20:28:19 -0300

    extrair dados, exibir e manipular

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -17,12 +20,19 @@ public class Main {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
         System.out.println(body);
+
         //extrair so os dados que interessam (titulo, poster, clasificacao)
-
-
+        var parser = new JsonParser();
+        List<Map<String, String>> listDeFilmes = parser.parse(body);
 
         //exibir e manipular os dados
-
+        for (Map<String, String> filme : listDeFilmes
+             ) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 
 }
