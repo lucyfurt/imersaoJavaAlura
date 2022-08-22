9cd37d904eb9e061f01b7f6657ab5d72da094477
lucyfurt <lucianafurtado10@gmail.com>
2022-08-22 16:09:54 -0300

    project springboot

---
+++ aula 3 e 4/lingugens-api/src/main/java/com/alura/lingugens/LingugensApiApplication.java
@@ -0,0 +1,13 @@
+package com.alura.lingugens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingugensApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingugensApiApplication.class, args);
+	}
+
+}
---
+++ aula 3 e 4/lingugens-api/src/test/java/com/alura/lingugens/LingugensApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.lingugens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingugensApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
